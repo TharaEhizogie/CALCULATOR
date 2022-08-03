c8da3a2753aea9caaedb1234cba0fe089342fe18
TharaEhizogie <tharaomo@gmail.com>
2022-08-03 22:24:55 +0100

    done with calculator

---
+++ index.js
@@ -0,0 +1,20 @@
+function addition() {
+  var num1 = window.prompt("Enter a number");
+  var num2 = window.prompt("Enter another number");
+  alert("Your answer is " + parseFloat(num1) + parseFloat(num2));
+}
+function subtraction() {
+  var num1 = window.prompt("Enter a number");
+  var num2 = window.prompt("Enter another number");
+  alert("Your answer is " + parseFloat(num1) - parseFloat(num2));
+}
+function multiplication() {
+  var num1 = window.prompt("Enter a number");
+  var num2 = window.prompt("Enter another number");
+  alert("Your answer is " + parseFloat(num1) * parseFloat(num2));
+}
+function division() {
+  var num1 = window.prompt("Enter a number");
+  var num2 = window.prompt("Enter another number");
+  alert("Your answer is " + parseFloat(num1) / parseFloat(num2));
+}
